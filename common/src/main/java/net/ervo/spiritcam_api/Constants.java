73ee4e94ded6024892f43105b7065cb5d93ce05e
Guillermo Ramos Alberti <agamenon1224@gmail.com>
2025-11-11 11:12:37 +0100

    First commit: setup of the project and Exposure imported

---
+++ common/src/main/java/net/ervo/spiritcam_api/Constants.java
@@ -0,0 +1,11 @@
+package net.ervo.spiritcam_api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "spiritcam_api";
+	public static final String MOD_NAME = "Exposure: Spirit Camera API";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ forge/src/main/java/net/ervo/spiritcam_api/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package net.ervo.spiritcam_api.platform;
+
+import net.ervo.spiritcam_api.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
